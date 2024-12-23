eeb9f0c9b2ca2c755bc69e5030cc7cd8fe4cc5d6
200201018 <130766354+isaArpat-dev@users.noreply.github.com>
2024-12-23 21:56:14 +0300

    a
    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Firebase'i başlatıyoruz.
   runApp(const MyApp());
 }
 
